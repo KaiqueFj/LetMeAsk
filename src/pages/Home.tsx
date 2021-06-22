0ad13f2723653f9f1ebc2552d3730da08da1b4b0
KaiqueFj <kaiquelferraz@Hotmail.com>
2021-06-22 13:48:38 -0300

    adding some features

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Ilustrationimg from '../assets/images/illustration.svg'
+
+export function Home() {
+    return (
+        <div>
+            <aside>
+                <img src={Ilustrationimg} alt='' />
+            </aside>
+        </div>
+
+    )
+
+}
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -9,7 +9,7 @@ const firebaseConfig = {
     databaseURL: process.env.REACT_APP_DATABASE_UR,
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGIN_SENDER_ID ,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID ,
     appId: process.env.REACT_APP_APP_ID ,
 };
 
